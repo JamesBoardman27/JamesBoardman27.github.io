e6089d08f66969c04cb038b84ef07a4065121a18
James Boardman <james.boardman@student.manchester.ac.uk>
2023-12-31 12:38:42 +0000

    Update page content

--- jamesboardman27-github-io/src/App.js
+++ jamesboardman27-github-io/src/App.js
@@ -5,7 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, my name is James. <br/>
+          I am a Computer Science student who is in his third and final year of study at The University of Manchester. <br/>
+          I am also a software engineer who has undertaken internships building EdTech web applications with my university and building autonomous vehicles with Five AI and Bosch.
         </p>
       </header>
     </div>
