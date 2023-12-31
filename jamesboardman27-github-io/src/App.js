18989ae4342863de63d282de4e9635e10af850c3
James Boardman <james.boardman@student.manchester.ac.uk>
2023-12-31 23:43:36 +0000

    Addition of Bootstrap carousel element

--- jamesboardman27-github-io/src/App.js
+++ jamesboardman27-github-io/src/App.js
@@ -4,11 +4,39 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hi, my name is James. <br/>
-          I am a Computer Science student who is in his third and final year of study at The University of Manchester. <br/>
-          I am also a software engineer who has undertaken internships building EdTech web applications with my university and building autonomous vehicles with Five AI and Bosch.
-        </p>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <h1>Hi, my name is James.</h1>
+            <p>
+              I am a Computer Science student who is in his third and final year of study at The University of Manchester. <br/>
+              I am also a software engineer who has undertaken a number of internships during my studies. <br/>
+              In 2022, I built an EdTech web application for one of The University of Manchester's programming course modules. <br/>
+              In 2023, I worked on cutting-edge autonomous vehicle technology with Five AI and Bosch. <br/>
+              To find out more about my experience and education, click the right arrow button!
+            </p>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Second slide"/>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="..." alt="Third slide"/>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+        </div>
       </header>
     </div>
   );
--- jamesboardman27-github-io/src/index.js
+++ jamesboardman27-github-io/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js'
+import App from './App';
+
+function SetPageTitle() {
+  useEffect(() => {
+    document.title = "James Boardman's Portfolio";
+  }, []);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
